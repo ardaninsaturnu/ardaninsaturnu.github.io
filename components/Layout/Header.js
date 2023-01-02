5c3ebb1f0b96be53c1d0962e913e1eaa5f225399
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-01-02 22:19:57 +0300

    header created. @main :tada:

---
+++ components/Layout/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return(
+    <>
+      <div>
+        <p>MehmetArdaCelik</p>
+        <div>
+          <span>About</span>
+          <span>Blog</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header;
