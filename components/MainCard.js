5c95e3970b9b8fa436d318acfb4e1c84936bfbe4
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-02-24 08:05:25 +0300

    fix with sm and 2xl

--- components/MainCard.js
+++ components/MainCard.js
@@ -6,14 +6,14 @@ const MainCard = () => {
   return(
     <>
       <main className="h-5/6 w-full bg-gray-900 flex justify-center items-center">
-      <div className="flex max-w-[1200px]">
+      <div className="flex sm:max-w-[700px] 2xl:max-w-[1200px]">
         <div>
           <Image src={arda} alt="Mehmet Arda Çelik"/>
         </div>
         <div className="text-white w-2/3 flex flex-col gap-3 pl-5">
-          <h1 className="text-5xl font-semibold">hey, I am Arda.</h1>
-          <h1 className="text-4xl">I like to develop products.</h1>
-          <p className="text-3xl text-zinc-400">
+          <h1 className="sm:text-3xl 2xl:text-5xl font-semibold">hey, I am Arda.</h1>
+          <h1 className="sm:text-2xl 2xl:text-4xl">I like to develop products.</h1>
+          <p className="sm:text-1xl 2xl:text-3xl text-zinc-400">
             Currently, I explore the world of AI.
             Before that, I worked as a frontend developer and ui designer.
             I like to capture moments through photography. Keyboard addicted.
