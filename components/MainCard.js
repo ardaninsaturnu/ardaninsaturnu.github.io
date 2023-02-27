8fb990ccb57210a9d3c77e37e9b2e7294c22ed0a
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-02-27 07:46:59 +0300

    main card fixed for mobile

--- components/MainCard.js
+++ components/MainCard.js
@@ -6,11 +6,11 @@ const MainCard = () => {
   return(
     <>
       <main className="h-5/6 w-full bg-gray-900 flex justify-center items-center">
-      <div className="flex sm:max-w-[700px] 2xl:max-w-[1200px]">
-        <div>
+      <div className="flex flex-col md:flex-row sm:max-w-[700px] 2xl:max-w-[1200px]">
+        <div className="w-3/4 mx-auto">
           <Image src={arda} alt="Mehmet Arda Çelik"/>
         </div>
-        <div className="text-white w-2/3 flex flex-col gap-3 pl-5">
+        <div className="text-white w-full md:w-2/3 flex flex-col gap-3 p-3 md:pl-5">
           <h1 className="sm:text-3xl 2xl:text-5xl font-semibold">hey, I am Arda.</h1>
           <h1 className="sm:text-2xl 2xl:text-4xl">I like to develop products.</h1>
           <p className="sm:text-1xl 2xl:text-3xl text-zinc-400">
