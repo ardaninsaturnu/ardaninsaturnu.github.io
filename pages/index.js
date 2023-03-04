1df451dece72417d9d7e0fdce9080ea480fc45de
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-03-04 23:45:45 +0300

    delete command

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      { /* TODO: make this website responsive as well */ }
       <div className="h-screen">
           <MainCard/>
           <FooterCard/>
